fb78730400bd14464a6617e20d5b61e5e6624778
Raido <oras.raido@gmail.com>
2025-12-10 04:40:08 +0200

    Day 1 Challenge 1

---
+++ 2025/Day1/DayOne.cs
@@ -0,0 +1,34 @@
+using advent_of_code._2025;
+using advent_of_code._2025.Day1;
+
+namespace advent_of_code
+{
+    public class DayOne
+    {
+        [Test]
+        public void ChallengeOne()
+        {
+            var dial = new Dial();
+            var input = DayOneInputs.FirstInput.Split('\n');
+            var password = 0;
+
+            foreach (var line in input) {
+                var trimmed = line.Trim();
+                if (trimmed.Length == 0) continue;
+
+                var direction = trimmed.Substring(0, 1);
+                var times = Int32.Parse(trimmed.Substring(1, trimmed.Length - 1));
+
+                if (direction == "L")
+                {
+                    if (dial.TurnLeft(times) == 0) password++;
+                }
+                else
+                {
+                    if (dial.TurnRight(times) == 0) password++;
+                }
+            }
+            Console.WriteLine(password);
+        }
+    }
+}
---
+++ 2025/Day1/Dial.cs
@@ -0,0 +1,29 @@
+﻿namespace advent_of_code._2025
+{
+    internal class Dial
+    {
+        private int indicator = 50;
+
+        public int TurnRight(int times)
+        {
+            //discard full circles
+            indicator += times % 100;
+            WrapIndicator();
+            return indicator;
+        }
+
+        public int TurnLeft(int times)
+        {
+            indicator -= times % 100;
+            WrapIndicator();
+            return indicator;
+        }
+
+        public void WrapIndicator()
+        {
+            // 0-99 is a 100 numbers.
+            if (indicator < 0) indicator += 100;
+            if (indicator > 99) indicator -= 100;
+        }
+    }
+}
