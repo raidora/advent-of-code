5b8a8d30c5a19d527fd97253a8d241befbcade50
Raido <oras.raido@gmail.com>
2025-12-15 01:13:33 +0200

    Day 2 Challenge 1

---
+++ 2025/Day2/DayTwo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace advent_of_code._2025.Day2
+{
+    internal class DayTwo
+    {
+
+        [Test]
+        public void ChallengeOne()
+        {
+            long invalidIDSum = 0;
+
+            var inputs = DayTwoInput.FirstInput
+                .Split(',')
+                .Select(i => i.Trim())
+                .ToList();
+
+            Parallel.ForEach(inputs, i =>
+            {
+                var range = i.Split('-');
+                var start = long.Parse(range[0]);
+                var end = long.Parse(range[1]);
+
+                for(var j = start; j <= end; j++)
+                {
+                    var jStr = j.ToString();
+                    var midPoint = jStr.Length / 2;
+
+                    if (jStr.Length % 2 != 0) continue;
+                    if (jStr[..midPoint] == jStr[midPoint..])
+                    {
+                        invalidIDSum += j;
+                    }
+                }
+            });
+
+            Console.WriteLine(invalidIDSum);
+        }
+    }
+}
